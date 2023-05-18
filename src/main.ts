fa9c31639585f8e3a8c60ee3da67566d901da2bd
Kravchenko Daniil <kravchenkodanil122@gmail.com>
2023-05-18 19:51:47 +0300

    add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app.module';
+import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+
+import { AppModule } from '@modules/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+  const port = process.env.PORT || 3100;
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+  );
+
+  app.enableCors({
+    origin: new RegExp(process.env.COOKIE_DOMAIN),
+    credentials: true,
+  });
+  app.setGlobalPrefix(globalPrefix);
+  app.use(helmet());
+  app.use(compression());
+  app.use(cookieParser());
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Kanban API')
+      .setDescription('')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  await app.listen(port, '0.0.0.0', () => {
+    Logger.log(`Listening at http://localhost:${port}/${globalPrefix}`);
+  });
 }
 bootstrap();
